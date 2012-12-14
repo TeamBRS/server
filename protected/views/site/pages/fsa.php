f185fa40060c396b48cf048d194994f4eb61b781
bernien2s <b.sexton@warwick.ac.uk>
2012-12-14 18:07:32 +0000

    added fsa menu option and view file

---
+++ protected/views/site/pages/fsa.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - FSA';
+$this->breadcrumbs=array(
+	'FSA',
+);
+?>
+<h2>Look for what you're after!</h2>
+<h4>Let us know what you want and we'll show you where to go and where to avoid!<h4>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
