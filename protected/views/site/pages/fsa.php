c7e3ce1932a2779d5a6940d423e4046fceb0fb85
bernien2s <b.sexton@warwick.ac.uk>
2012-12-14 19:18:02 +0000

    gets location

--- protected/views/site/pages/fsa.php
+++ protected/views/site/pages/fsa.php
@@ -9,5 +9,48 @@ $this->breadcrumbs=array(
 <h2>Look for what you're after!</h2>
 <h4>Let us know what you want and we'll show you where to go and where to avoid!<h4>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<!--FSA Search Form, will eventually be based on UM options -->
+<div class="form">
+	<div class="row">
+          <label for="inputid">Location</label>
+          <input name="locationid" id="locationid" type="text" />
+          <p class="hint" onclick="getLocation()">Get Current Location</p>
+    </div>
+    <div class="row">
+          <label for="inputid">Minimum Rating</label>
+          <input name="inputid" id="inputid" type="text" />
+          <p class="hint">Don't get caught out by poor restaurant standards!</p>
+    </div>
+    <div class="row buttons">
+          <label for="inputid">Cuisine</label>
+          <input name="inputid" id="inputid" type="text" />
+          <p class="hint">What do you fancy?</p>
+    </div>
+    <div class="row buttons">
+          <label for="inputid">Want to know the buzz?</label>
+          <input name="inputid" id="inputid" type="text" />
+          <p class="hint">Find out what others are saying.</p>
+    </div>
+</div>
+
+<script type="text/javascript">
+var tbox=document.getElementById("locationid");
+
+function getLocation()
+{
+	if (navigator.geolocation)
+    {
+    	navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  	else
+  	{
+  		tbox.value = "undefined";
+  	}
+}
+
+function showPosition(position)
+{
+	tbox.value =  position.coords.latitude + ", " + position.coords.longitude; 
+}
+
+</script>
