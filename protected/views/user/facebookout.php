0c6b4953fcf2cde5ac7b8cd2fd283c377a5535ff
Robin Janssens <robin.janssens@gmail.com>
2013-01-05 16:02:19 +0000

    redirect page created

---
+++ protected/views/user/facebookout.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this FacebookController */
+
+$this->breadcrumbs=array(
+	'Facebook',
+);
+?>
+<h1>Please wait while we redirect you to the Facebook website</h1>
+
+<?php echo $message; ?>
+
+
